6259f7c82618080a6bcb18f9c46fa57e2a8f3ed3
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-04-27 15:22:28 +0700

    27/04/2020

---
+++ src/ConvertCToF.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class ConvertCToF {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("input F:");
+                    fahrenheit=scanner.nextDouble();
+                    System.out.println("C= "+fahrenheitToCelsius(fahrenheit));
+                    break;
+                case 2:
+                    System.out.println("input C:");
+                    celsius=scanner.nextDouble();
+                    System.out.println("F= "+celsiusToFahrenheit(celsius));
+                    break;
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+}
